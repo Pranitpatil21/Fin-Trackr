d63494960c2a780819bc15886330c7a808cb91a2
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-01-13 00:38:54 +0530

    add_transaction_screen.dart

--- fin_trackr/lib/screens/add_transaction_screen.dart
+++ fin_trackr/lib/screens/add_transaction_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:uuid/uuid.dart'; 
 
-import '../models/transaction_model.dart';
+import '../models/transaction_model.dart'; 
 import '../blocs/transaction/transaction_bloc.dart';
 import '../blocs/transaction/transaction_event.dart'; 
 
